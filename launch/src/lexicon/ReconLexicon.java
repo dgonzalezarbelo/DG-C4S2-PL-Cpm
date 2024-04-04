9c78cde949e6e0d99940223aeaeddf5a5b53ed62
LAPTOP-JD\jdbar <jdbarradodaganzo@gmail.com>
2024-04-04 11:32:05 +0200

    Solucionados los errores en el lexico: - Se reconocen los comentarios multilinea - Se han cambiado la definicion del simbolo para el operador asignacion Solucionados los errores de configuracion del proyecto en VSCode: - La clase principal para ejecutar la aplicacion es Main.java y en ella hay que escribir el codigo de lo que se pretenda - El fichero "launch.json" contiene las directrices de ejecucion y depurado (separadas). Es importante que en "mainClass" este la ruta al NOMBRE de la clase que se quiere considerar como clase de entrada a la aplicacion, considerando la ruta relativa desde el directorio especificado como directorio de archivos fuente.

--- launch/src/lexicon/ReconLexicon.java
+++ launch/src/lexicon/ReconLexicon.java
@@ -7,9 +7,7 @@ import java.io.Reader;
 import java_cup.internal_error;
 
 public class ReconLexicon {
-  public static final String[] args = {"-cp", "cup.jar", "-parser", "AnalizadorSintacticoTiny", "-symbols", "ClaseLexica", "-nopositions", "Tiny.cup"};
   public static void main(String[] args) throws internal_error, Exception {
-    java_cup.Main.main(ReconLexicon.args);  // Esta línea lanza JFLEX para regenerar la clase del analizadorLexico
     Reader input = new InputStreamReader(new FileInputStream(args[0]));
     AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
     LexiconUnit unidad;
--- launch/src/syntax/ReconSyntax.java
+++ launch/src/syntax/ReconSyntax.java
@@ -7,10 +7,10 @@ import java.io.Reader;
 import lexicon.AnalizadorLexicoTiny;
 
 public class ReconSyntax {
-   public static void main(String[] args) throws Exception {
-     Reader input = new InputStreamReader(new FileInputStream(args[0]));
-	 AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
-	 AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);
-	 asint.parse();
- }
+	public static void main(String[] args) throws Exception {
+		Reader input = new InputStreamReader(new FileInputStream(args[0]));
+		AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
+		AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);
+		asint.parse();
+	}
 }   
