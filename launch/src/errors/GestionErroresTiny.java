97113326ebc4634d8776cde84bb5c0ce8635078c
Daniel Gonzalez Arbelo <dgonzarb@gmail.com>
2024-05-12 14:58:35 +0200

    Merge con lo de Juandi

--- launch/src/errors/GestionErroresTiny.java
+++ launch/src/errors/GestionErroresTiny.java
@@ -1,19 +1,22 @@
 package errors;
 
 import lexicon.LexiconUnit;
+import mainApp.Main;
 import utils.Utils;
 
 public class GestionErroresTiny {
 	public void errorLexico(int row, int col, String lexema) {
-		System.out.println("Lexicon error: Caracter " + lexema + " inesperado");
+		Main.errorDetected = true;
+		System.out.println("Lexicon error: unexpected character " + lexema);
 		Utils.printErrorRowCol(row, col);
 	}
 
 	public void errorSintactico(LexiconUnit unidadLexica) {
+		Main.errorDetected = true;
 		if (unidadLexica.lexema() != null)
-			System.out.println("Sintax error: Elemento inesperado \"" + unidadLexica.lexema() + "\"");
+			System.out.println("Syntax error: unexpected character '" + unidadLexica.lexema() + "'");
 		else
-			System.out.println("Sintax error: Elemento inesperado");
+			System.out.println("Syntax error: unexpected character");
 		Utils.printErrorRowCol(unidadLexica.row(), unidadLexica.col());
 	}
 }
